a5737b812bd2a297b9f8cc10e232b6cc06bc54a4
Abokina Wilson-Jones <abo_jones@hotmail.co.uk>
2020-02-27 17:35:44 +0000

    Updates

---
+++ precache-manifest.d1bcd2725e71ea9bc2bf05824de99b4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1aee28041ceca199d729b99374b9a31",
+    "url": "/cards-list/index.html"
+  },
+  {
+    "revision": "3fd64c528a5580a14b1b",
+    "url": "/cards-list/static/css/main.d86ce3ba.chunk.css"
+  },
+  {
+    "revision": "9209bb1c34e312b9e29b",
+    "url": "/cards-list/static/js/2.9920e411.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/cards-list/static/js/2.9920e411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fd64c528a5580a14b1b",
+    "url": "/cards-list/static/js/main.b7dcffe3.chunk.js"
+  },
+  {
+    "revision": "10e58d3774bee80de57d",
+    "url": "/cards-list/static/js/runtime-main.33aa3880.js"
+  }
+]);
